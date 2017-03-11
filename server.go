ab3f6f70e172445256dc82dccdfdd36a78edcfbc
toshiki <yz.t0.r33@gmail.com@>
2017-03-11 22:41:37 +0900

    ルートだけでOK！

--- server.go
+++ server.go
@@ -13,13 +13,5 @@ func main() {
         return c.String(http.StatusOK, "Hello, World!")
     })
 
-    e.GET("/users/:id", func(c echo.Context) error {
-        jsonMap := map[string]string{
-            "name": "toshiki",
-            "hoge": "hoge",
-        }
-        return c.JSON(http.StatusOK, jsonMap)
-    })
-
     e.Logger.Fatal(e.Start(":8080"))
 }
