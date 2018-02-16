73f86b31cb9d4a63701817a3a96457e4f70b0b8b
toshiki0525 <toshiki.baba@atrae.co.jp>
2018-02-16 20:38:35 +0900

    vue.js tutorial始めるよ

--- app/javascript/packs/test/index.js
+++ app/javascript/packs/test/index.js
@@ -4,5 +4,4 @@ import App from '../../app.vue'
 document.addEventListener('DOMContentLoaded', () => {
   document.body.appendChild(document.createElement('hello'))
   const app = new Vue({ render: h => h(App) }).$mount('hello')
-  console.log(app)
 })
