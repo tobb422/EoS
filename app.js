1d5630403733184517fa1afc873931837e533491
Toshiki Baba <yz.t0.r33@gmail.com>
2018-04-22 04:42:53 +0900

    基本的な使い方

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
-import indexRouter from './routes/index';
+// import indexRouter from './routes/index';
 
 const app = express();
 
@@ -21,19 +21,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', express.static('views'));
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
+// app.use((req, res, next) => {
+//   next(createError(404));
+// });
 
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use((err, req, res, next) => {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,8 +1,55 @@
 import Vue from 'vue';
 
+const componentData = {
+  test: 'テストだよ'
+}
+
+const Child = {
+  template: '<div>{{ myMessage }}A custom child-component!{{ test }}</div>',
+  props: ['myMessage'],
+  data: function() {
+    return componentData;
+  }
+}
+
 const vm = new Vue({
   el: '#app',
   data: {
-    message: 'Hello'
+    msg: 'Hello',
+    isActive: false,
+    ok: true,
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ],
+    object: {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30
+    },
+    counter: 0,
+    message: "test",
+    checked: false,
+    checkedNames: [],
+    picked: '',
+    selected: ''
+  },
+  computed: {
+    // 算出 getter 関数
+    reversedMessage: function () {
+      // `this` は vm インスタンスを指します
+      return this.message.split('').reverse().join('');
+    }
+  },
+  methods: {
+    add: function() {
+      this.counter += 1;
+    },
+    say: function(msg) {
+      alert(msg);
+    }
+  },
+  components: {
+    'my-component': Child
   }
 })
